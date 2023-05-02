452e597ab5573de35b56dfc60ca219bf9ec09a63
Jess <jessgatura@gmail.com>
2023-05-02 12:37:54 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @str: the string to get length of
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	size_t length = 0;
+
+	while (*str++)
+	{
+		length++;
+	}
+	return (length);
+}
