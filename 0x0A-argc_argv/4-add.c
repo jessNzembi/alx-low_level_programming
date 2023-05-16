bbc9de713424fb1483515ec623546e50ee6a5494
Jess <jessgatura@gmail.com>
2023-05-16 05:27:53 -0700

    4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0 (success)
+ */
+
+int main(int argc, char **argv)
+{
+	int sum = 0;
+	int i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (*argv[i] >= 48 && *argv[i] <= 57)
+			{
+				sum += atoi(argv[i]);
+			}
+			else 
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+	return (0);
+}
