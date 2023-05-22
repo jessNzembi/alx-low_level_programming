cce5cdf91ee76d0409951a04b7a12530a23c6543
Jess <jessgatura@gmail.com>
2023-05-22 12:26:55 -0700

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs
+ * @d: dog to be freed
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+		free(d);
+}
