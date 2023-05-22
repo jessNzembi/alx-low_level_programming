47d3fa508c3a67ba9ea57992fd40e2ebdf44adbd
Jess <jessgatura@gmail.com>
2023-05-22 10:55:16 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - represents a dog
+ * @name: dog's name
+ * @owner: dog's owner
+ * @age: dog's age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
