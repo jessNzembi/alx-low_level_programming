0ab3fa67615d81a19793ec999408c4f2e67c4fb7
Jess <jessgatura@gmail.com>
2023-05-24 04:45:14 -0700

    fixed some errors

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -17,17 +17,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		if (size <= 0)
 			return (-1);
-		else
+		for (i = 0; i < size; i++)
 		{
-			for (i = 0; i < size; i++)
-			{
-				result = cmp(array[i]);
-				if (result != 0)
-					break;
-			}
-			if (result == 0)
-				return (-1);
+			result = cmp(array[i]);
+			if (result != 0)
+				break;
 		}
+		if (result == 0)
+			return (-1);
 	}
 	else
 	{
