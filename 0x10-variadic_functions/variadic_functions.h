95de399d0bd79b3f6b18489a99d36abefc0633d2
Jess <jessgatura@gmail.com>
2023-05-25 02:14:16 -0700

    sum them all

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,12 @@
+#ifndef _VAR_H_
+#define _VAR_H_
+
+int sum_them_all(const unsigned int n, ...);
+
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+void print_strings(const char *separator, const unsigned int n, ...);
+
+void print_all(const char * const format, ...);
+
+#endif
