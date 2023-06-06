fc221f99c9a58623dd89490a471a8aac8b6a488f
Jess <jessgatura@gmail.com>
2023-06-06 11:50:50 -0700

    insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,30 +4,55 @@
  * insert_nodeint_at_index - inserts a new node at a given position
  * @head: pointer
  * @idx: index of the list where the new node should be added
- * @n: data to be added 
+ * @n: data to be added
  * Return: the address of the new node
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new, *tmp = *head;
-	unsigned int i;
+	listint_t *new, *prev;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
-	
-	for (i = 0; i < idx - 1; i++)
-	{
-		if (tmp == NULL)
-			return (NULL);
-		tmp = tmp->next;
-	}
 	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
+	if (!new)
 		return (NULL);
 	new->n = n;
-	new->next = tmp->next;
-	tmp->next = new;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	prev = get_nodeint_at_index(*head, idx - 1);
+	if (prev == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = prev->next;
+	prev->next = new;
 	return (new);
+}
+
+/**
+ * get_nodeint_at_index - gets the nth node
+ * @head: pointer
+ * @index: n
+ * Return: pointer to nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *ptr = head;
+	unsigned int count = 0;
+
+	while (ptr)
+	{
+		if (count == index)
+			return (ptr);
+		count++;
+		ptr = ptr->next;
+	}
+	return (NULL);
 }
